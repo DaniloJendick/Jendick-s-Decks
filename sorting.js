a30f0cc80a70818a0f1f146116e66951ee2ab5be
DaniloJendick <42503051+DaniloJendick@users.noreply.github.com>
2022-07-02 15:28:42 +0100

    Add files via upload

--- sorting.js
+++ sorting.js
@@ -6,7 +6,9 @@ let sentences = [
     "i will use these to put the sentences on anki",
     "to memorize and learn languages",
     "these is intended to be impoted to anki",
-    "i also use morphman to find sentences with only one word that i don't know"
+    "I also use morphman to find sentences with only one word that i don't know",
+    "For Fuck's sake's",
+    "aoisdhoiashd's"
 ];
 
 function sorting(sentences){
@@ -21,43 +23,28 @@ function sorting(sentences){
     }
     map = [...map.entries()].sort((a, b) => b[1] - a[1]);
     //Ranking
-    for(let i = 0; i < map.length; i++){ map[i][1] = i+1;}
+    for(let i = 0; i < map.length; i++){ map[i][1] = 1/map[i][1];}
+    //console.log(map)
     map = new Map(map)
-    //console.log(map);
-
-    let result = [];
-    let senTemp = "";
-    let indexes = [];
-    let done = false;
-    let done2 = false;
-    let doing;
-    let points = 0;
-    while(!done){
-        if(sentences.length == 0){done = true; break;}
-        doing = sentences[sentences.length-1].replace(/[^a-zA-Z0-9 ||']/, "").replace("  "," ").toLowerCase().split(" ")
-        senTemp = sentences[sentences.length-1];
-        indexes = [];
-        done2 = false;
-        while(!done2){
-            if(doing.length == 1){done2 = true;}
-            indexes.push(   parseInt(map.get(doing[doing.length-1]))  );
-            //console.log("doing: ")
-            //console.log(doing);
-            doing.pop();
-            
+    
+    let sentence = [];
+    let word = ""; 
+    let max = "";
+    for(let i = 0; i < sentences.length; i++){
+        sen = sentences[i].replace(/[^a-zA-Z0-9 ||']/, "").replace("  "," ").split(" ");
+        for(let j = 0; j < sen.length; j++){
+            if(max < map.get(sen[j].toLowerCase())){
+                 max = map.get(sen[j].toLowerCase());
+                 word = sen[j]; 
+            }
         }
-        //console.log(indexes);
-        points = indexes.reduce((total, value) => total + value);
-        result.push([senTemp, indexes.reverse(), points]);
-        sentences.pop();
+        max = 0;
+        sentence.push(sentences[i].replace( word , "{{c1::"+word+"::"+word.replace(/[a-zA-Z0-9 ||']/gi, "*")+"}}"))
+        
+    
     }
-    result = result.sort((a, b) => b[2] - a[2]);
-
-    //anki Friendly input TSV tab-delimited
-    for(let i = 0; i < result.length; i++){
-        //console.log(result[i][0]+ "\t" +result[i][1] + "\t" + result[i][2])
-    }
-    return result;
+    sentence.map(e => console.log(e))
+    return sentence;
     
 }
 
